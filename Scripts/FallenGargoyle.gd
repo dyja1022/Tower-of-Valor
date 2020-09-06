a2fd6490f86a35fc4f44d04c0ddcaee445d7d8ed
Jameson Dy <dyjameson22@gmail.com>
2020-09-06 16:08:30 -0700

    gargoyles target closest player

--- Scripts/FallenGargoyle.gd
+++ Scripts/FallenGargoyle.gd
@@ -33,11 +33,25 @@ func throw_spear():
 		return
 	if has_spear:
 		if players.size() > 0:
-			players.shuffle()
-			if players[0] != null:
-				spear.set_target(players[0].global_position)
-				has_spear = false
+			pick_closest_target()
 
+func pick_random_target():
+	players.shuffle()
+	if players[0] != null:
+		spear.set_target(players[0].global_position)
+		has_spear = false
+
+func pick_closest_target():
+	players = get_tree().get_nodes_in_group("player")
+	var closest_dist = 10000
+	var closest_targ = Vector2()
+	for p in players:
+		var dist = global_position.distance_to(p.position)
+		if dist < closest_dist:
+			closest_targ = p.global_position;
+			closest_dist = dist			
+	spear.set_target(closest_targ)
+	has_spear = false
 
 func _on_ReloadSpear_timeout():
 	if !has_spear:
--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 onready var lives_lbl : Label = $Lives1
+onready var lives_lbl2 : Label = $Lives2
 
 func _on_Player_update_health(health : int):
 	lives_lbl.text = "HP: " + str(health)
+
+func _on_Player2_update_health(health : int):
+	lives_lbl2.text = "HP: " + str(health)
